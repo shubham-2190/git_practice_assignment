6347b2745d1cb83ce08cced5d4ad34f01b0846f0
shubham dhakad <shubham975414@gmail.com>
2022-12-06 18:46:45 +0530

    i have done my task

---
+++ login.js
@@ -0,0 +1,15 @@
+//Problem 1 : Check whether a number is Prime or not
+
+
+let num=13;
+let count =0;
+for (let i=1;i<=num;i++){
+if(num%i==0){
+count++;
+}
+}
+if(count==2){
+cunsole.log(num, :is a prime number");
+} else {
+console.log(num,"is not a prime number");
+}
